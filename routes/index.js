5bd1847776f1c0a61c97b828d92b4f750338bd9f
cblake003 <cameronburtner@gmail.com>
2023-08-17 17:46:58 -0700

    added delete function

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 // redirects always make a GET request
-  res.redirect('/skills');
+  res.redirect('/skills', {title: 'Home Page'});
 });
 
 module.exports = router;
